d5d3199c48ca70f48069b33379a18239ae51ab17
Andrea Ranieri <andrea.ranieri@cnr.it>
2024-11-08 13:54:13 +0100

    Made the TRAIN_multi_image_batch.py script a bit more user friendly, now training with bs=63 (bs>64 always gives error, can't understand why) and saving models with "meaningful" names only when loss improves...

--- TRAIN_multi_image_batch.py
+++ TRAIN_multi_image_batch.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
 # Train/Fine Tune SAM 2 on LabPics 1 dataset
 # This mode use several images in a single batch
 # Labpics can be downloaded from: https://zenodo.org/records/3697452/files/LabPicsV1.zip?download=1
 
+import os
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
 import numpy as np
 import torch
 import cv2
-import os
 
 from torch.onnx.symbolic_opset11 import hstack
 
@@ -14,7 +18,8 @@ from sam2.sam2_image_predictor import SAM2ImagePredictor
 
 # Read data
 
-data_dir=r"LabPicsV1//" # Path to dataset (LabPics 1)
+#data_dir=r"LabPicsV1//" # Path to dataset (LabPics 1)
+data_dir=r"/mnt/raid1/dataset/LabPicsV1/"
 data=[] # list of files in dataset
 for ff, name in enumerate(os.listdir(data_dir+"Simple/Train/Image/")):  # go over all folder annotation
     data.append({"image":data_dir+"Simple/Train/Image/"+name,"annotation":data_dir+"Simple/Train/Instance/"+name[:-4]+".png"})
@@ -58,7 +63,7 @@ def read_single(data): # read random image and single mask from  the dataset (La
         yx = np.array(coords[np.random.randint(len(coords))]) # choose random point/coordinate
         return Img,mask,[[yx[1], yx[0]]]
 
-def read_batch(data,batch_size=4):
+def read_batch(data, batch_size=4):
       limage = []
       lmask = []
       linput_point = []
@@ -83,52 +88,64 @@ predictor = SAM2ImagePredictor(sam2_model)
 predictor.model.sam_mask_decoder.train(True) # enable training of mask decoder
 predictor.model.sam_prompt_encoder.train(True) # enable training of prompt encoder
 predictor.model.image_encoder.train(True) # enable training of image encoder: For this to work you need to scan the code for "no_grad" and remove them all
-optimizer=torch.optim.AdamW(params=predictor.model.parameters(),lr=1e-5,weight_decay=4e-5)
-scaler = torch.cuda.amp.GradScaler() # mixed precision
+
+optimizer = torch.optim.AdamW(params=predictor.model.parameters(), lr=1e-5, weight_decay=4e-5)
+scaler    = torch.cuda.amp.GradScaler() # mixed precision
 
 # Training loop
 
+best_loss = float("inf")
+
 for itr in range(100000):
-    with torch.cuda.amp.autocast(): # cast to mix precision
-            image,mask,input_point, input_label = read_batch(data,batch_size=4) # load data batch
-            if mask.shape[0]==0: continue # ignore empty batches
-            predictor.set_image_batch(image) # apply SAM image encoder to the image
-            # predictor.get_image_embedding()
-            # prompt encoding
+	with torch.cuda.amp.autocast():							# cast to mix precision
+		image, mask, input_point, input_label = read_batch(data, batch_size=63)	# load data batch
 
-            mask_input, unnorm_coords, labels, unnorm_box = predictor._prep_prompts(input_point, input_label, box=None, mask_logits=None, normalize_coords=True)
-            sparse_embeddings, dense_embeddings = predictor.model.sam_prompt_encoder(points=(unnorm_coords, labels),boxes=None,masks=None,)
+		if mask.shape[0]==0:
+			continue							# ignore empty batches
 
-            # mask decoder
+		predictor.set_image_batch(image)					# apply SAM image encoder to the image
+		# predictor.get_image_embedding()
+		# prompt encoding
 
-            high_res_features = [feat_level[-1].unsqueeze(0) for feat_level in predictor._features["high_res_feats"]]
-            low_res_masks, prd_scores, _, _ = predictor.model.sam_mask_decoder(image_embeddings=predictor._features["image_embed"],image_pe=predictor.model.sam_prompt_encoder.get_dense_pe(),sparse_prompt_embeddings=sparse_embeddings,dense_prompt_embeddings=dense_embeddings,multimask_output=True,repeat_image=False,high_res_features=high_res_features,)
-            prd_masks = predictor._transforms.postprocess_masks(low_res_masks, predictor._orig_hw[-1])# Upscale the masks to the original image resolution
+		mask_input, unnorm_coords, labels, unnorm_box = predictor._prep_prompts(input_point, input_label, box=None, mask_logits=None, normalize_coords=True)
+		sparse_embeddings, dense_embeddings = predictor.model.sam_prompt_encoder(points=(unnorm_coords, labels), boxes=None, masks=None)
 
-            # Segmentaion Loss caclulation
+		# mask decoder
 
-            gt_mask = torch.tensor(mask.astype(np.float32)).cuda()
-            prd_mask = torch.sigmoid(prd_masks[:, 0])# Turn logit map to probability map
-            seg_loss = (-gt_mask * torch.log(prd_mask + 0.00001) - (1 - gt_mask) * torch.log((1 - prd_mask) + 0.00001)).mean() # cross entropy loss
+		high_res_features = [feat_level[-1].unsqueeze(0) for feat_level in predictor._features["high_res_feats"]]
+		low_res_masks, prd_scores, _, _ = predictor.model.sam_mask_decoder(image_embeddings=predictor._features["image_embed"], image_pe=predictor.model.sam_prompt_encoder.get_dense_pe(),sparse_prompt_embeddings=sparse_embeddings,dense_prompt_embeddings=dense_embeddings,multimask_output=True,repeat_image=False,high_res_features=high_res_features,)
+		prd_masks = predictor._transforms.postprocess_masks(low_res_masks, predictor._orig_hw[-1])# Upscale the masks to the original image resolution
 
-            # Score loss calculation (intersection over union) IOU
+		# Segmentaion Loss caclulation
 
-            inter = (gt_mask * (prd_mask > 0.5)).sum(1).sum(1)
-            iou = inter / (gt_mask.sum(1).sum(1) + (prd_mask > 0.5).sum(1).sum(1) - inter)
-            score_loss = torch.abs(prd_scores[:, 0] - iou).mean()
-            loss=seg_loss+score_loss*0.05  # mix losses
+		gt_mask  = torch.tensor(mask.astype(np.float32)).cuda()
+		prd_mask = torch.sigmoid(prd_masks[:, 0])# Turn logit map to probability map
+		seg_loss = (-gt_mask * torch.log(prd_mask + 0.00001) - (1 - gt_mask) * torch.log((1 - prd_mask) + 0.00001)).mean() # cross entropy loss
 
-            # apply back propogation
+		# Score loss calculation (intersection over union) IOU
 
-            predictor.model.zero_grad() # empty gradient
-            scaler.scale(loss).backward()  # Backpropogate
-            scaler.step(optimizer)
-            scaler.update() # Mix precision
+		inter = (gt_mask * (prd_mask > 0.5)).sum(1).sum(1)
+		iou = inter / (gt_mask.sum(1).sum(1) + (prd_mask > 0.5).sum(1).sum(1) - inter)
+		score_loss = torch.abs(prd_scores[:, 0] - iou).mean()
+		loss = seg_loss + score_loss*0.05  # mix losses
 
-            if itr%1000==0: torch.save(predictor.model.state_dict(), "model.torch") # save model
+		# apply back propogation
 
-            # Display results
+		predictor.model.zero_grad() # empty gradient
+		scaler.scale(loss).backward()  # Backpropogate
+		scaler.step(optimizer)
+		scaler.update() # Mix precision
 
-            if itr==0: mean_iou=0
-            mean_iou = mean_iou * 0.99 + 0.01 * np.mean(iou.cpu().detach().numpy())
-            print("step)",itr, "Accuracy(IOU)=",mean_iou)
+		# Display results
+
+		if itr == 0:
+			mean_iou = 0
+		mean_iou = mean_iou * 0.99 + 0.01 * np.mean(iou.cpu().detach().numpy())
+		print(f"step: {itr} - accuracy (IOU): {mean_iou:.2f} - loss: {loss:.2f} - seg_loss: {seg_loss:.2f} - score_loss: {score_loss:.2f} - best_loss: {best_loss:.2f}")
+		if itr % 10 == 0:
+			if loss < best_loss:
+				# save the model
+				best_loss = loss
+				model_str = f"{sam2_checkpoint.replace('.pt','')}-step-{itr}-acc-{mean_iou:.2f}-best-loss-{loss:.2f}-segloss-{seg_loss:.2f}-scoreloss-{score_loss:.2f}.pth"
+				torch.save(predictor.model.state_dict(), model_str);
+
